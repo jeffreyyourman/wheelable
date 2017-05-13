8688942eb6ed370492424d4e97d9532f2dc7da3a
Craig Ramey <craigramey@gmail.com>
2017-05-13 18:36:52 -0400

    fixed models

--- models/Location.js
+++ models/Location.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Location = new Schema({
-  name: String,
-  street:  String,
-  zip: Integer,
-  city: String,
-  state: String,
-  country: String,
+const LocationSchema = new Schema({
+  name: { type: String },
+  street: { type: String },
+  zip: { type: Number },
+  city: { type: String },
+  state: { type: String },
+  country: { type: String },
   accessibleFriendly: Boolean,
-  reason: String
+  reason: { type: String }
 });
 
+const Location = mongoose.model("Location", LocationSchema);
+
 module.exports = Location;
+
--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const User = new Schema({
-  title:  String,
+const UserSchema = new Schema({
+  name:  { type: String }
 });
 
 module.exports = User;
 
-
+const User = mongoose.model("User", UserSchema);
 
