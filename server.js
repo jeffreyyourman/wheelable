acbae666c7a9dd4dee4a6eedf6202a2e7abcebc4
Craig Ramey <craigramey@gmail.com>
2017-05-13 18:28:11 -0400

    added public folder, skeleton of form

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.render('home', {foo: 'Hi'});
 });
+
+app.get('/form', (req, res) => {
+  res.render('form')
+})
+
 
 app.listen(PORT);
