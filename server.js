a3bc82909096355190f4694b4944dfa18e5ce13f
jeffreyyourman <jeffreyyourman@gmail.com>
2017-05-13 21:50:25 -0400

    pubnub activated

--- server.js
+++ server.js
@@ -11,48 +11,48 @@ const Location = require('./models/Location.js');
 const User = require('./models/User.js');
 const sms = require('./controller/sms');
 
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect(`mongodb://localhost/wheelable`);
-}
-
-db.on("error", (error) => {
-  console.log(`Mongoose Error: ${error}`);
-});
-
-db.once("open", () => {
-  console.log(`Mongoose connection successful.`);
-  //var foo = new User({name: "Craig"})
-  //var bar = new User({name: "Jon"})
-  //User.insertMany([foo, bar], (res) => {
-  // console.log(res)
-  //})
-  var foo = new Location({
-    name: "Fuck off",
-    street: "River Rd",
-    zip: "07020",
-    city: "Edgewater",
-    country: "USA",
-    accessibleFriendly: false,
-    reason: "No clue"
-  })
-
-  var bar = new Location({
-    name: "Fuck off",
-    street: "River Rd",
-    zip: "07020",
-    city: "Edgewater",
-    country: "USA",
-    accessibleFriendly: false,
-    reason: "No clue"
-  })
-
-  Location.insertMany([foo, bar], (res) => {
-    console.log(res)
-  })
-
-});
+// if (process.env.MONGODB_URI) {
+//   mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//   mongoose.connect(`mongodb://localhost/wheelable`);
+// }
+//
+// db.on("error", (error) => {
+//   console.log(`Mongoose Error: ${error}`);
+// });
+//
+// db.once("open", () => {
+//   console.log(`Mongoose connection successful.`);
+//   //var foo = new User({name: "Craig"})
+//   //var bar = new User({name: "Jon"})
+//   //User.insertMany([foo, bar], (res) => {
+//   // console.log(res)
+//   //})
+//   var foo = new Location({
+//     name: "Fuck off",
+//     street: "River Rd",
+//     zip: "07020",
+//     city: "Edgewater",
+//     country: "USA",
+//     accessibleFriendly: false,
+//     reason: "No clue"
+//   })
+//
+//   var bar = new Location({
+//     name: "Fuck off",
+//     street: "River Rd",
+//     zip: "07020",
+//     city: "Edgewater",
+//     country: "USA",
+//     accessibleFriendly: false,
+//     reason: "No clue"
+//   })
+//
+//   Location.insertMany([foo, bar], (res) => {
+//     console.log(res)
+//   })
+//
+// });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -66,10 +66,13 @@ app.get('/form', (req, res) => {
   res.render('form')
 })
 
+app.get('/map', (req, res) => {
+  res.render('map')
+})
+
 
 app.use('/sms', sms);
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log(PORT)
 });
-
