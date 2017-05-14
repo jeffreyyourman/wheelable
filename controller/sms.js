664df7259df0f552ed8f53fa8701117057a58821
William Vasquez <william.a.vasquez@gmail.com>
2017-05-14 08:39:38 -0400

    testing sms

--- controller/sms.js
+++ controller/sms.js
@@ -12,7 +12,7 @@ module.exports = (function() {
   let tText = req.query.text;
   handleWebhook(req.query, res);
 
-  request('https://vast-ridge-24155.herokuapp.com//location/484 hillcrest rd. new york, ny', function (error, response, body) {
+  request('https://vast-ridge-24155.herokuapp.com/location/484 hillcrest rd. new york, ny', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred 
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
   console.log('body:', body); // Print the HTML for the Google homepage. 
