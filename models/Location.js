0c9c8b7a8dfdda7cf492496d0ec790ad8079bd84
jeffreyyourman <jeffreyyourman@gmail.com>
2017-05-14 01:30:02 -0400

    form and schema adjustment

--- models/Location.js
+++ models/Location.js
@@ -3,12 +3,14 @@ const Schema = mongoose.Schema;
 
 const LocationSchema = new Schema({
   name: { type: String },
-  address: { type: String}, 
+  address: { type: String},
   accessibleFriendly: Boolean,
+  accessibleRamp: Boolean,
+  accessibleStairs: Boolean,
   reason: { type: String }
 });
+
 
 const Location = mongoose.model("Location", LocationSchema);
 
 module.exports = Location;
-
