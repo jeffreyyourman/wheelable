49229e972e3193609936bb6b0473e7b7f8e0ef08
Craig Ramey <craigramey@gmail.com>
2017-05-13 22:04:35 -0400

    fixed static pathing issues

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ db.once("open", () => {
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
